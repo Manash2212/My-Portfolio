76ef3f2312e200c0a595211f48eb3aa18a25a2de
Manash <manashhalder2212@gmail.com>
2024-01-14 21:48:57 +0530

    Created Service Component and pusihng to git hub

---
+++ my-portfolio/my-portfolio/src/component/hero/Hero.jsx
@@ -0,0 +1,86 @@
+import "../../styles/hero.scss";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const textVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1,
+      },
+    },
+
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+
+  const sliderVariants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: "-300%",
+      opacity: 1,
+      transition: {
+        duration: 20,
+        repeat: Infinity,
+        repeatType: "mirror",
+      },
+    },
+  };
+
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>MANASH HALDER</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web developer and UI designer
+          </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the letest works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            src="/scroll.png"
+            alt="scroll-logo"
+            variants={textVariants}
+            animate="scrollButton"
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Coder | Content Creater | Fitness Freak
+      </motion.div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
